31a14828a34961874af695a367a031d9944aead6
lyna-heng <elynahenggrundy@gmail.com>
2025-11-10 01:10:54 +0100

    final?

--- app/main.jsx
+++ app/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "./root";
+import { BrowserRouter } from "react-router-dom";
+import { RootWrapper } from "./root";
 import "./app.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Root />
+    <BrowserRouter basename="/Serenote-Webapp">
+      <RootWrapper />
+    </BrowserRouter>
   </React.StrictMode>
 );

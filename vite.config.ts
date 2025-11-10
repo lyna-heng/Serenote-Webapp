2a34ec74433d68188f5b7750960e4c94664d7fd8
lyna-heng <elynahenggrundy@gmail.com>
2025-11-10 01:45:16 +0100

    :)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: '/Serenote-Webapp/',
-  plugins: [tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
